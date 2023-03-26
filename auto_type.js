6bd965d40ccf0855c8055a355a6eb09e62699415
Anish Kothari <107585475+ani0722@users.noreply.github.com>
2023-03-26 13:25:23 +0530

    Add files via upload

---
+++ auto_type.js
@@ -0,0 +1,9 @@
+
+var options = {
+  strings: [" Where love blooms", "100% True Love , 0% catfish"],
+  typeSpeed: 70,
+  backSpeed: 30,
+  loop: true,
+};
+
+var typed = new Typed(".slogan-app", options); 
